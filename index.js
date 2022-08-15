fa45b2d96b13c0c33a1bdf82f289a136029c4017
muamarzidan <zidanprofix93@gmail.com>
2022-08-15 15:08:28 +0700

    1st

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 var link = "https://reqres.in/api/users";
 
+// this code to compare how usefull axios than fetch
+
+
+//when get
 fetch(link+'/1')
     .then((response)=>response.json()).then((data) => {
         console.log(data.data);
@@ -12,6 +16,7 @@ axios.get(link+'/1')
     .then((data) => console.log(data.data))
     .catch((error) => console.log('error : ' + error));
 
+//when post
 fetch(link, {
     method: 'post',
     headers: {
@@ -32,8 +37,34 @@ axios.post(link, {
 })
 .then((data) => console.log(data));
 
+//when delete
+fetch(link+'/1', {
+    method: 'delete'
+}).then((response) => response.json()).then((data) => {
+    console.log(data);
+});
+
+axios.delete(link+'/1').then((data) => console.log(data));
+
+//when put
+fetch(link+'/1', {
+    method: 'put',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+        name: 'Omiru',
+        job: 'coders'
+
+    })
+    }).then((response) => response.json()).then((data) => {
+    console.log(data);
+    });
+
+
+//when error 
 fetch(link+'/x')
 .then((response) => console.log(response)).catch((error) => console.log('error : ' + error));
 
-axios.get(link+'/x').then((response) => console.log(response))
-.catch((error) => console.log('error : ' + error));
+axios.get(link+'/x')
+.then((response) => console.log(response)).catch((error) => console.log('error : ' + error));
